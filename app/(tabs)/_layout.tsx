7eb2e7b1f2248c4674f9eca6597b0af6d5021b13
Vikrant-1 <vikrantkainth090699@gmail.com>
2024-10-12 09:43:53 +0530

    create tab routes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,11 +24,11 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="expenses"
         options={{
-          title: 'Explore',
+          title: 'Expense',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={focused ? 'wallet' : 'wallet-outline'} color={color} />
           ),
         }}
       />
