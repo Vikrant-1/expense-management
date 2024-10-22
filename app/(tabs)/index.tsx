e1c522c818d74cb4e61073dc8bc121b613d35bc0
Vikrant-1 <vikrantkainth090699@gmail.com>
2024-10-22 21:38:43 +0530

    fix watchman issue

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,9 @@
-import Auth from "@/components/Auth";
 import { StyleSheet, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View>
-      <Auth />
+
     </View>
   );
 }
